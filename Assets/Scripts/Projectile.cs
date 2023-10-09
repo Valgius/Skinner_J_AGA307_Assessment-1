8fe2c88f259bf2ff048932749f5837c78efc1991
Valgius <jax.skinner@hotmail.com>
2023-10-09 20:21:04 +1100

    Basic Aim and collisions

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Check to see if the collided object has the tag "Target"
+        if (collision.collider.CompareTag("Target"))
+        {
+            //Change the collided objects material colour to red
+            collision.collider.GetComponent<Renderer>().material.color = Color.red;
+            //Destroy the collided object after 1 second
+            Destroy(collision.collider.gameObject, 1f);
+            //Destroy this gameobject
+            Destroy(this.gameObject);
+        }
+    }
+        
+}
