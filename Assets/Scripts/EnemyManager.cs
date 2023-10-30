12e7197d0ef7072ae1dc5e621ff92527a72dd863
Valgius <jax.skinner@hotmail.com>
2023-10-30 23:14:34 +1100

    Brief 3 Requirements

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] enemyTypes;
+
+    public List<GameObject> enemies;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+            SpawnAtRandom();
+    }
+
+    void SpawnAtRandom()
+    {
+        int rndEnemy = Random.Range(0, enemyTypes.Length);
+        int rndSpawn = Random.Range(0, spawnPoints.Length);
+        GameObject enemy = Instantiate(enemyTypes[rndEnemy], spawnPoints[rndSpawn].position, spawnPoints[rndSpawn].rotation);
+        enemies.Add(enemy);
+    }
+}
